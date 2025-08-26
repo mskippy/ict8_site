922575b7adc2b6c57c5d0a718f5da16d69a6f81a
Michelle Ahoy <19247@sd44.ca>
2025-08-26 16:29:33 -0700

    Update js script

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,4 +7,12 @@ document.querySelectorAll('a[href]').forEach(a => {
     if (here === location.pathname) a.setAttribute('aria-current', 'page');
   });
   console.info("ICT 8 loaded with sidebar");
-  
+
+  // highlight the current page in the left nav (handles spaces in filenames)
+(function () {
+  const here = decodeURI(location.pathname.split('/').pop() || 'index.html').toLowerCase();
+  document.querySelectorAll('#sidebar .navlist a').forEach(a => {
+    const target = decodeURI(a.getAttribute('href').split('/').pop()).toLowerCase();
+    if (target === here) a.classList.add('active');
+  });
+})();
