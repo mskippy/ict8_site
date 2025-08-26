e81494d0c9ca86f11ecf9967b5c277efc260e269
Michelle Ahoy <19247@sd44.ca>
2025-08-26 12:35:13 -0700

    Added unit index pages and update navigation bar on root index

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,8 @@
 // lightweight enhancements go here
 console.info("ICT 8 loaded");
+
+document.querySelectorAll('header nav a').forEach(a => {
+    const url = new URL(a.getAttribute('href'), location);
+    if (url.pathname === location.pathname) a.setAttribute('aria-current', 'page');
+  });
+  
